22ac582e9284f3b7393ef236036b2426e8319ec6
AlexanderSanes <44125554+AlexanderSanes@users.noreply.github.com>
2018-10-17 01:20:28 +0300

    Игнорирование регистра при вводе

--- Test/main.cpp
+++ Test/main.cpp
@@ -6,6 +6,11 @@
 #include <fstream>
 
 using namespace std;
+int StrToInt(string str)
+{
+	int num = 1;
+	return num;
+}
 void toLower(basic_string<char>& s) {
 	for (basic_string<char>::iterator p = s.begin(); p != s.end(); ++p) {
 		*p = tolower(*p);
@@ -23,33 +28,46 @@ int main(int argc, char const *argv[])
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	string str;
-	string key, equal;
-	ifstream in;
-	in.open("FileOut.txt");
-	map <string, string> book;
+	string str{ "Hello World!" };
+	int num;
+	num = StrToInt(str);
+	cout << "num = " << num << endl;
 
-	while (in)
-	{
-	getline(in, key);
-	getline(in, equal);
-	book.insert(make_pair(key, equal));
-	}
 
-	for (map <string, string>::iterator i = book.begin(); i != book.end(); i++)
-	{
-	cout << "Переведите : " << i->first << ':';
-	cin >> str;
-	toLower(str);
-	if (str == i->second)
-	{
-		cout << "Правильно!!!(нажмите enter, чтобы продолжить...)";
-		cin.get();
-		cin.get();
-	}
-	else
-	cout << " Неправильно!!! Правильный перевод : " << i->second << endl ;
-	}
+
+
+
+	//string str;
+	//string key, equal;
+	//ifstream in;
+	//in.open("FileOut.txt");
+	//map <string, string> book;
+	//int n = 10;
+	//string num;
+	//int k;
+
+	//for (int i = 0; i <= n || in; ++i)
+	//{
+	//getline(in, num);
+	//getline(in, key);
+	//getline(in, equal);
+	//book.insert(make_pair(key, equal));
+	//}
+
+	//for (map <string, string>::iterator it = book.begin(); it != book.end(); it++)
+	//{
+	//cout << "Переведите : " << it->first << ':';
+	//cin >> str;
+	//toLower(str);
+	//if (str == it->second)
+	//{
+	//	cout << "Правильно!!!(нажмите enter, чтобы продолжить...)";
+	//	cin.get();
+	//	cin.get();
+	//}
+	//else
+	//cout << " Неправильно!!! Правильный перевод : " << it->second << endl ;
+	//}
 
 	system("pause");
 	return 0;
